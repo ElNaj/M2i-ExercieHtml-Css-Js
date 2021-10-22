5fb435d1384ff24931dda9869bb713eb1ecdb661
ElNaj <nelfateoui@gmail.com>
2021-10-22 12:02:14 +0200

    Premier commit pour la gallerie Img en cours de réalisation

---
+++ scripts/image.js
@@ -0,0 +1,3 @@
+
+let image = document.querySelector("#img1");
+image.src = "https://img-19.ccm2.net/cI8qqj-finfDcmx6jMK6Vr-krEw=/1500x/smart/b829396acc244fd484c5ddcdcb2b08f3/ccmcms-commentcamarche/20494859.jpg";
